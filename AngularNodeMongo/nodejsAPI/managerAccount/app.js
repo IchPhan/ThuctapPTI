c13181473bd0250a1820e5c4b8c9dc1b97100fb6
IchPhan <47978090+IchPhan@users.noreply.github.com>
2019-10-03 18:08:22 +0700

    Connect and get data from api

--- AngularNodeMongo/nodejsAPI/managerAccount/app.js
+++ AngularNodeMongo/nodejsAPI/managerAccount/app.js
@@ -3,7 +3,7 @@ const BodyParser = require("body-parser");
 const mongoose = require("mongoose");
 var url = "mongodb://localhost:27017/";
 var dbname = "AccountDB";
-var collec = "account2";
+var collec = "accounts";
 var app = Express();
 // var AccountModel=require("./model/accountModel").AccountModel;
 
@@ -32,10 +32,10 @@ var account = new Schema({
     address: String
 });
 // Biên dịch mô hình từ schema
-var accountModel = mongoose.model(collec, account);
+var accountModel = mongoose.model(collec, account,"accounts");
 app.post("/api/account/create", async (req, res) => {
-    
-    var myobj =new accountModel({
+
+    var myobj = new accountModel({
         fullname: req.body.fullname,
         phone: req.body.phone,
         email: req.body.email,
@@ -43,44 +43,48 @@ app.post("/api/account/create", async (req, res) => {
         birthday: req.body.birthday,
         address: req.body.address
     });
-    myobj.save((err)=>{
+    myobj.save((err,accounts) => {
         if (err) throw err;
+        res.status(200).json(result=accounts);
     });
-    res.send("Your object is null");
 })
-app.put("/api/account/update/:id", async (request, response) => {
-
+app.put("/api/account/update/:id", async (req, res) => {
+    var myobj = new accountModel({
+        fullname: req.body.fullname,
+        phone: req.body.phone,
+        email: req.body.email,
+        sex: req.body.sex,
+        birthday: req.body.birthday,
+        address: req.body.address
+    });
+    accountModel.findById(req.body.id,(res,accounts)=>{
+        accounts.updateOne(myobj);
+    });
 })
-app.get("/api/account", async (request, response) => {
+app.get("/api/account", async (req, res) => {
+    //accountModel.findOne({});
+    
+    accountModel.findOne().exec((err,accounts)=>{
+        res.send(result=accounts);
 
+    });
 })
 app.get("/api/account/all", async (request, response) => {
-    accountModel.find((err,res)=>{
-        if(err) throw err;
+    accountModel.find((err, accounts) => {
+        if (err) throw err;
         console.log(res);
     });
 })
 app.delete("/api/account/delete/:id", async (request, response) => {
-
+   // accountModel.deleteOne();
 })
 app.get("/api", async (request, response) => {
     console.log("That not good")
-    accountModel.findOne((err,res)=>{
-        if(err) throw err;
+    accountModel.findOne((err, res) => {
+        if (err) throw err;
         console.log(res.account.fullname);
     });
 })
-var server = app.listen(8001, "localhost", () => {
+var server = app.listen(9999, "localhost", () => {
     db.on;
-    var myobj =new accountModel({
-        fullname:"Phan ich95",
-        phone: "0988676",
-        email: "phanich95@gmail.com",
-        sex: true,
-        birthday: "2019-10-13",
-        address: "viet nam"
-    });
-    myobj.save((err)=>{
-        if (err) throw err;
-    });
 });
